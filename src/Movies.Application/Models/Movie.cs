bbbfc13a7d63388be969654a3963060ccd085955
mr1compiler <mr1compiler@gmail.com>
2025-06-05 01:27:06 +0300

    Adding Slug-based retrieval

--- src/Movies.Application/Models/Movie.cs
+++ src/Movies.Application/Models/Movie.cs
@@ -1,9 +1,22 @@
+using System.Text.RegularExpressions;
+
 namespace Movies.Application.Models;
 
-public class Movie
+public partial class Movie
 {
     public required Guid Id { get; init; }
     public required string Title { get; set; }
+    public string Slug => GenerateSlug();
     public required int YearOfRelease { get; set; }
     public required IEnumerable<string> Genres { get; set; } = Enumerable.Empty<string>();
+    
+    private string GenerateSlug()
+    {
+        var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+            .ToLower().Replace(" ", "-");
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
+
+    [GeneratedRegex("[^0-9A-Za-z_-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
 }
