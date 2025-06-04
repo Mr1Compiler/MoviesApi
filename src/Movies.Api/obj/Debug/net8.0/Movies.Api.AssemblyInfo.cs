d4f79f33f42d0b0c2d12a67ce8f500ea52139df6
mr1compiler <mr1compiler@gmail.com>
2025-06-04 11:47:57 +0300

    Add Interfaces, Implementationand Injection

--- src/Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
+++ src/Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e01f301bc13a6c750be053a440a9cf954798ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86aa00c5238fce0f09430b8a06d11f0fc515d9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
